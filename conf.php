f7502f76fb01630a033037653b8217a3a98fe92a
Asfak Ansari <vowelweb18@gmail.com>
2018-01-05 20:21:24 +0530

    adding shopify code

---
+++ conf.php
@@ -0,0 +1,7 @@
+<?php
+
+	define('SHOPIFY_SHOP', 'chat-fuel-teststore.myshopify.com');
+	define('SHOPIFY_APP_API_KEY', '4d71037d20b246981fae2a03af8babbf');
+	define('SHOPIFY_APP_PASSWORD', '8b117a661daaa228ff1d532975a954b2');
+
+?>
--- index.php
+++ index.php
@@ -1,57 +1,110 @@
 <?php
-//DB Credential
-	$servername = "localhost";
-	$username = "root";
-	$password = "password";
-	$dbname = "postmen";
 
-// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+	session_start();
 
-// Check connection
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
+	require __DIR__.'/vendor/autoload.php';
+	use phpish\shopify;
+	require __DIR__.'/conf.php';
+	
+	function updateTracking($ref,$trakingId){
+
+		$shopify = shopify\client(SHOPIFY_SHOP, SHOPIFY_APP_API_KEY, SHOPIFY_APP_PASSWORD, true);
+		
+			try{
+				
+				 $orderId = $shopify('GET /admin/orders.json?name='.$ref);
+				 $checkoutOrderId=$orderId[0]['id'];
+				if($checkoutOrderId!='')
+				{
+					$updateTrakingId = $shopify('POST /admin/orders/'.$checkoutOrderId.'/fulfillments.json',array(),array
+					('fulfillment'=>array
+						(
+						'tracking_number'=>$trakingId
+						
+						)
+		
+					));
+				
+			
+				}
+				
+				
+			}
+				catch (shopify\ApiException $e)
+			{
+			try{
+		 	$shopify = shopify\client($_GET['shop'], SHOPIFY_APP_API_KEY, $oauth_token );
+		 	$orderId = $shopify('GET /admin/orders.json?name=1002');
+			$checkoutOrderId=$orderId[0]['id'];
+			if($checkoutOrderId!=''){
+			$updateTrakingId = $shopify('POST /admin/orders/'.$checkoutOrderId.'/fulfillments.json',array(),array
+			('fulfillment'=>array
+				(
+					'tracking_number'=>'1234567890'
+					
+				)
+	
+			));
+			
+			
+			}
+				
+				
+				}
+				catch (shopify\ApiException $e)
+			{
+				# HTTP status code was >= 400 or response contained the key 'errors'
+				//echo $e;
+				print_r($e->getRequest());
+				print_r($e->getResponse());
+				echo '{"code":100,"msg":"Something went wrong. please try after some time."}';
+				exit;
+			}
+			catch (shopify\CurlException $e)
+			{
+				# cURL error
+				//echo $e;
+				//print_r($e->getRequest());
+				print_r($e->getResponse());
+				echo '{"code":100,"msg":"Something went wrong. please try after some time."}';
+				exit;
+			}
+	
+				# HTTP status code was >= 400 or response contained the key 'errors'
+				//echo $e;
+				print_r($e->getRequest());
+				print_r($e->getResponse());
+				echo '{"code":100,"msg":"Something went wrong. please try after some time."}';
+				exit;
+			}
+			catch (shopify\CurlException $e)
+			{
+				# cURL error
+				//echo $e;
+				//print_r($e->getRequest());
+				print_r($e->getResponse());
+				echo '{"code":100,"msg":"Something went wrong. please try after some time."}';
+				exit;
+			}
+
+	
 	}
 
-//Receive the RAW post data via the php://input IO stream.
 	$content = file_get_contents("php://input");
 	$datas = json_decode($content, true);
-
-//Fetch Tracking ID
-	$tracking_ids = $datas['data']['tracking_numbers'];
+	//Fetch Tracking ID
+	$tracking_ids = $datas['data']['tracking_numbers'][0];
 	$tracking_id = '';
 	foreach ($tracking_ids as $value) {
 		# code...
 		$tracking_id .= $value.' , ';
 	}
 
-//Check For canpar
-	$is_canpar = $datas['data']['rate']['shipper_account']['slug'];
-
-//Reference Id
-	$refs = $datas['data']['references'];
-	$ref = '';
-	foreach ($refs as $ref_value) {
-		# code...
-		$ref .= $ref_value.' , ';
-		
-	}
-
-//Current Time
-	$now = date('Y-m-d H:i:s');
-
-
-	$sql = "INSERT INTO order_data set tracking_id = '".$tracking_id."',shipper = '".$is_canpar."',order_id = '".$ref."',created_at = '".$now."'";
+	//Check For canpar
+	//$is_canpar = $datas['data']['rate']['shipper_account']['slug'];
+	$ref = $datas['data']['references'][0];
 	
-		if ($conn->query($sql)) {
-		    echo '{"code":200,"message":"success"}';
-		    exit;
-		} else {
-		    echo '{"code":202,"message":"fail"}';
-		    exit;
-		}
-	
-	
-
+	updateTracking($ref,$tracking_ids);
+	//updateTracking('1004','TestOne146554');
 
 ?>
